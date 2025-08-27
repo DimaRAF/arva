0d0809e021f7d1ede74236d38acb3f3ad57be93f
DimaRAF <d.r.abolehia@gmail.com>
2025-08-27 12:37:19 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; 
 import 'screens/splashscreen.dart';
-//
+
 // The main entry point of the application.
 
 void main() async {
