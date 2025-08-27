8c08e245f483556949e26440f11e2e71abc4ac94
DimaRAF <d.r.abolehia@gmail.com>
2025-08-27 13:31:19 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'screens/splashscreen.dart';
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
-  
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
